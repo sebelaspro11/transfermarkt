c34e1ebcc957c2bf661d8dc0fa1bb55eb2e4ebb6
sebelaspro11 <132761130+sebelaspro11@users.noreply.github.com>
2025-06-06 00:17:20 +0800

    Update team_network.py

--- code/team_network.py
+++ code/team_network.py
@@ -321,9 +321,8 @@ if not df.empty:
     
     nationality_count = df_flat["Nationality"].value_counts().reset_index()
     nationality_count.columns = ["Nationality", "Count"]
-    # nationality_count["Nationality"] = nationality_count["Nationality"].apply(country_to_flag_url)
-    st.dataframe(nationality_count)
-    st.markdown(nationality_count.to_html(escape=False, index=False), unsafe_allow_html=True)
+    nationality_count["Nationality"] = nationality_count["Nationality"].apply(generate_flag_html)
+
 
 
     # Recalculate Market Value and Age Group on original df
@@ -355,6 +354,7 @@ if not df.empty:
 
     with tab2:
         st.markdown(nationality_count.to_html(escape=False, index=False), unsafe_allow_html=True)
+        
 
     with tab3:
         df3 = position_count.reset_index(drop=True)
