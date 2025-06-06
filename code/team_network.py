d1376f69668d2ac3f9c4ac79d9ee6c6c91b5d6da
sebelaspro11 <132761130+sebelaspro11@users.noreply.github.com>
2025-06-07 00:26:42 +0800

    Update team_network.py

--- code/team_network.py
+++ code/team_network.py
@@ -9,7 +9,7 @@ import requests
 import pycountry
 
 # --- Streamlit Page Config ---
-st.set_page_config(page_title="Football Team Network", page_icon="⚽", layout="wide")
+st.set_page_config(page_title="Football Team Network", page_icon="⚽")
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
